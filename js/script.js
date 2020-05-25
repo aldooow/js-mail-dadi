26ee460762f9513ebd42bf0cccba5ad9d8dae44a
Aldo Cahuana <aldooow@icloud.com>
2020-05-25 19:09:01 +0200

    Work in progress!!!!

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-
-var emailForm = prompt(email);
-
+// Lista Email!!!!
+var emailForm = prompt("email");
+var alertForm = document.getElementById('js_alert');
 var email = ['pinco@aol.com', 'pallino@aol.com', 'lucky@aol.com', 'pinina@aol.com']
 var mailCorretta = false;
-
 
 for ( var i = 0; i < email.length; i++) {
    if (email[i] === emailForm) {
@@ -13,13 +12,38 @@ for ( var i = 0; i < email.length; i++) {
 
 if (mailCorretta) {
   alert('Email corretta, puo accedere');
+  alert.innerHTML = 'Email corretta, puo accedere';
 } else {
   alert('Mi dispiace, non puoi accedere');
-
+  alert.innerHTML = 'Mi dispiace, non puoi accedere';
 }
 
-// buttonProva.addEventListener('click',
-// function() {
-//
-//
-//  })
+
+// Gioco dei dadi!!!
+
+var buttonSi = document.getElementById('js_si');
+var buttonNo = document.getElementById('js_no');
+
+var hiddenUno = document.getElementById('js_hidden-1');
+var hiddenDue = document.getElementById('js_hidden-2');
+
+buttonSi.addEventListener('click',
+ function() {
+   hiddenDue.className = 'wrapper';
+ } )
+
+buttonNo.addEventListener('click',
+function() {
+  hiddenDue.className = 'wrapper hidden';
+} )
+
+var buttonVersus = document.getElementById('js_versus');
+var playerUtente = document.getElementById('js_utente');
+var playerComputer = document.getElementById('js_computer');
+var lama = document.getElementById('lama');
+buttonVersus.addEventListener('click',
+function() {
+  playerUtente.innerHTML = Math.floor(Math.random() * 6) + 1;
+  playerComputer.innerHTML = Math.floor(Math.random() * 6) + 1;
+
+})
